f4e04e2debdc9d5593a6e4ce80a0827c9b5c475e
James Shi <jamesshi117@gmail.com>
2022-10-27 14:05:48 -0700

    Add required phone number field and length validation

--- client/src/App.js
+++ client/src/App.js
@@ -21,37 +21,51 @@ export const App = () => {
     setSubscriberData({phoneNumber: '', username: '', password: '', domain: '', status: '', features: featureOne});
   };
 
-  const handleSubmit = async (e) =>{
-    switch (currentCommand){
-      case 'Get':
-        e.preventDefault();
-        const res = await getSubscriber(subscriberData.phoneNumber);
-        try{
-          console.log(res.features.callForwardNoReply);
-          setSubscriberData({phoneNumber: res.phoneNumber, username: res.username, password: res.password, domain: res.domain, status: res.status, features: { callForwardNoReply: {destination: res.features.callForwardNoReply.destination, provisioned: res.features.callForwardNoReply.provisioned}}});
-        } catch (error) {
-          console.log('Invalid phone number');
+  const handleSubmit = async (e) => {
+    if (subscriberData.phoneNumber.length >= 10) {
+      switch (currentCommand) {
+        case 'Get':
+          e.preventDefault();
+          const res = await getSubscriber(subscriberData.phoneNumber);
+          try {
+            console.log(res.features.callForwardNoReply);
+            setSubscriberData({
+              phoneNumber: res.phoneNumber,
+              username: res.username,
+              password: res.password,
+              domain: res.domain,
+              status: res.status,
+              features: {
+                callForwardNoReply: {
+                  destination: res.features.callForwardNoReply.destination,
+                  provisioned: res.features.callForwardNoReply.provisioned
+                }
+              }
+            });
+          } catch (error) {
+            console.log('Invalid phone number');
+            clear();
+          }
+          break;
+        case 'Put':
+          putSubscriber(subscriberData.phoneNumber, subscriberData);
           clear();
-        }
-        break;
-
-      case 'Put':
-        putSubscriber(subscriberData.phoneNumber, subscriberData);
-        clear();
-        break;
-
-      case 'Delete':
-        deleteSubscriber(subscriberData.phoneNumber);
-        clear();
-        break;
-
-      default: 
-        return;
+          break;
+        case 'Delete':
+          deleteSubscriber(subscriberData.phoneNumber);
+          clear();
+          break;
+        default:
+          return;
+      }
+      console.log(`Submitting form with command: ${currentCommand}`);
+      setCommand('Get');
+    } else {
+      e.preventDefault();
+      console.log('Invalid phone number');
     }
+  };
 
-    console.log(`Submitting form with command: ${currentCommand}`);
-    setCommand('Get');
-  }    
 
   return (
     <>
@@ -59,15 +73,11 @@ export const App = () => {
       <Paper className={classes.paper}>
         <form className={`${classes.root} ${classes.form}`} autoComplete="off" onSubmit={handleSubmit}>
           <Typography variant="h6">IMS Subscriber Panel</Typography>
-
-          <TextField fullWidth color="primary" name="phoneNumber" variant="outlined" label="Phone Number" value={subscriberData.phoneNumber} onChange={(e) => setSubscriberData({ ...subscriberData, phoneNumber: e.target.value })}/>
-
+          <TextField required fullWidth color="primary" name="phoneNumber" variant="outlined" label="Phone Number" value={subscriberData.phoneNumber} onChange={(e) => setSubscriberData({ ...subscriberData, phoneNumber: e.target.value })}/>
           <ActivableForm currentCommand={currentCommand} subscriberData={subscriberData} setSubscriberData={setSubscriberData} featureOne={featureOne} setFeatureOne={setFeatureOne}/>
-
           <Button className={classes.buttonSubmit} variant="contained" size="large" type="submit" fullWidth>Submit</Button>
         </form>
       </Paper>
-
       <Stack className={classes.optionBar} direction='row'>
         <GetButton currentCommand={currentCommand} setCommand={setCommand}/>
         <DeleteButton currentCommand={currentCommand} setCommand={setCommand}/>
